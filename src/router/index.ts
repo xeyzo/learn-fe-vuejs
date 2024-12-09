6d9cd075c77c15d37afd0cd5146e3a628e5560c3
xeyzo <schneidergw@yahoo.com>
2024-12-10 01:49:32 +0700

    init login component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import AboutView from "../views/AboutView.vue";
+import Login from "../views/Login.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -13,8 +15,12 @@ const routes: Array<RouteRecordRaw> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component:AboutView
+  },
+  {
+    path: "/login",
+    name: "login",
+    component:Login
   },
 ];
 
---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex">
+    <div class="container w-1/2 bg-gray-300">
+        <form action="" class="flex flex-col items-center justify-center h-screen">
+            <h1 class="text-6xl text-black">Login</h1>
+            <input type="text" placeholder="Email" class="mt-8 px-4 py-2 rounded-lg border-2 border-white w-1/2">
+            <input type="password" placeholder="Password" class="mt-8 px-4 py-2 rounded-lg border-2 border-white w-1/2">
+            <button class="mt-8 px-4 py-2 rounded-lg bg-white text-black w-1/2">Login</button>
+        </form>
+    </div>
+    <div class="container w-1/2">
+        
+    </div>
+    </div>
+</template>
